9914b25a8af41e1133134448ee7269ea118b522a
Roninthevictory <158804933+Roninthevictory@users.noreply.github.com>
2026-03-12 20:21:26 -0500

    Update server.js

--- server.js
+++ server.js
@@ -111,5 +111,5 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => console.log(`CinemaSync active on port ${PORT}`));
